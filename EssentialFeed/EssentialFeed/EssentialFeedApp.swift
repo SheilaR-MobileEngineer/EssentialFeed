b145767939e1d64eb0b1c86bbe49ca9f5c70a632
Sheila Rodriguez <sheila.rodriguez@dexcom.com>
2023-06-04 14:33:07 -0600

    first commit

---
+++ EssentialFeed/EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by sr0922 on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
