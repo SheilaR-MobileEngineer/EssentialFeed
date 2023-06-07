2729fdaa726c3b7942613eb19a56409b6b8ec346
SheilaR-MobileEngineer <ssromar@hotmail.com>
2023-06-06 19:07:20 -0600

    Request data from URL on 'load' as singleton

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTest.swift
@@ -9,19 +9,33 @@ import XCTest
 
 
 class RemoteFeedLoader {
-    
+    func load(){
+        HTTPClient.shared.requestedURL = URL(string: "MYURL.COM")
+    }
 }
 
 class HTTPClient {
+    static let shared = HTTPClient()
+    private init(){}
     var requestedURL: URL?
 }
+
 class RemoteFeedLoaderTest: XCTestCase {
     
     func test_init_doesNotRequestDatFromRUL() {
-        let client = HTTPClient()
+        let client = HTTPClient.shared
         _ = RemoteFeedLoader()
         
         XCTAssertNil(client.requestedURL)
+    }
+    
+    func test_load_requestDataFromURL(){
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+            
+        sut.load()
+
+        XCTAssertNotNil(client.requestedURL)
     }
 
 }
