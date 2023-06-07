2987aba555bb244dcda2f910d6974986fd88819a
SheilaR-MobileEngineer <ssromar@hotmail.com>
2023-06-06 18:31:54 -0600

    Project setup.

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Sheila Rodriguez on 6/6/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
